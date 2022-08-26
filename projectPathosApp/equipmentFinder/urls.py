f6ee40c7ecd08d4497cd2e48a0b6c4f254e5c86d
tryingNerd24 <chrishayden.cds@gmail.com>
2022-08-26 14:03:00 -0500

    Second Commit

--- projectPathosApp/equipmentFinder/urls.py
+++ projectPathosApp/equipmentFinder/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "equipmentFinder"
 urlpatterns = [
     path("equip-search/", views.searchPage, name='equipSearchPage'),
-    path("add-equip/", views.addEquipmentPage, name='addEquipmentPage')
+    path("add-equip/", views.addEquipmentPage, name='addEquipmentPage'),
+    path("search-results/", views.searchResultsPage, name='searchResultsPage')
 ]
--- projectPathosApp/projectPathosApp/urls.py
+++ projectPathosApp/projectPathosApp/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('project-pathos/', include('homePage.urls', namespace='home')),
     path('project-pathos/', include('equipmentFinder.urls', namespace='equipmentFinder')),
+    re_path(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
